c6d8698072e1be66979511cf36cb08138341c0d3
marutyan76 <city.mati76@gmail.com>
2018-09-12 11:45:06 +0900

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,13 +3,6 @@ filebucket { "main":
    path => false,
 }
 
-Package { allow_virtual => false }
-
-Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
-
-File { backup => "main" }
-
-
 node default inherits dev_sample {
    notify { "woof": message => "Did you define a node? This is the default node message." }
    include test_page
