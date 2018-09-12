28772b424de34043d012a3b4a11869931473bf96
marutyan76 <city.mati76@gmail.com>
2018-09-12 18:34:47 +0900

    test

--- modules/xylitol/manifests/init.pp
+++ modules/xylitol/manifests/init.pp
@@ -2,6 +2,8 @@ class xylitol {
     #import datadog
 
     case $ec2_instance_type {
-        't2.micro','t2.small' : {include xylitol::gum}
+        't2.micro' : {include xylitol::gum_process}
+        't2.micro' : {include xylitol::gum_tcp_chcek}
+        't2.micro','t2.small' : {include xylitol::gum_web_scraping}
     }
 }
