99360194833492a2c35c0a5d28ee514bc33e36b2
marutyan76 <city.mati76@gmail.com>
2018-09-12 18:56:22 +0900

    test

--- modules/xylitol/manifests/gum_process.pp
+++ modules/xylitol/manifests/gum_process.pp
@@ -8,5 +8,13 @@ class xylitol::gum_process {
         mode    =>  644,
         source  =>  "puppet:///modules/xylitol/process/devlive_process.yaml",
         }
+    }elsif $hostname == "oreo11" {
+        file { "process.yaml" :
+        path    =>  "/var/tmp/process.yaml",
+        owner   =>  root,
+        group   =>  root,
+        mode    =>  644,
+        source  =>  "puppet:///modules/xylitol/process/devlive_process.yaml",
+        }
     }
 }
--- modules/xylitol/manifests/init.pp
+++ modules/xylitol/manifests/init.pp
@@ -2,7 +2,7 @@ class xylitol {
     #import datadog
 
     case $hostname {
-        'test10' : {include xylitol::gum_process}
+        'test10','oreo11' : {include xylitol::gum_process}
         'test10' : {include xylitol::gum_tcp_check}
         'test10','oreo11' : {include xylitol::gum_web_scraping}
     }
